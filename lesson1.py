f66974369f9f353b2bfdb5093d6e3601a1f7892b
OrestBlynda <borest979@gmail.com>
2024-12-01 17:41:53 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,86 @@
+#print("Hello World")
+
+#Тип змінноЇ int, цілі числа
+#number = 10
+
+#Тип змінноЇ float, не цілі числа
+#price = 9.99
+
+#Тип змінноЇ String, рядок або текст
+#name  = "Valentine"
+
+#Тип змінноЇ bool(boolean), True або False(1 або 0)
+#light = True
+
+
+#sprite = 1.34
+#cocacola = 2.99
+
+#name = "Orest"
+#surname = "Blynda"
+
+# light = False
+# night = True
+
+# print(sprite)
+# print(cocacola)
+# print(name)
+# print(surname)
+# print(light)
+# print(night)
+
+
+# watermelon = 90
+# print(type(watermelon))
+
+
+# fanta = 1.56
+# print(type(fanta))
+
+# number = 200
+# print(type(number))
+
+# nickname = Fantastic
+# print(type(Fantastic))
+
+# night = True
+# print(type(night)) 
+
+# tymophi = 3
+# matvii = 3
+
+# if tymophi > 6:
+#     print("12 points tymophi")
+# elif matvii > 6:
+#     print("12 points matvii")
+# else:
+#     print("boys have 2 points")
+
+# danylo = 8
+# orest = 7
+
+# if danylo < 10:
+#     print("4 sweets danylo")
+# elif orest < 10:
+#     print("4 sweets orest")
+# else:
+#     print("boys have 0 sweet")
+
+
+for i in range(10):
+    print("Hello World!")
+
+a = 5
+
+while a < 10:
+    a += 1
+    print(a)
+
+for i in range(5):
+    print("Orest Blynda!")
+
+b = 7 
+
+while b < 10:
+    b += 1
+    print(b)
